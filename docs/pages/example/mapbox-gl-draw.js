b1cd2023825702eb5337c0fa678c9cb0ed9a729c
Fan <42430149+winters113@users.noreply.github.com>
2018-08-21 10:54:32 +0800

    CN Translation done 100%
    CN Translation done 100%

--- docs/pages/example/mapbox-gl-draw.js
+++ docs/pages/example/mapbox-gl-draw.js
@@ -1,8 +1,7 @@
 /*---
-title: Show drawn polygon area
+title: 显示绘制区域
 description: >-
-  Use GL draw to draw a polygon and turf.js to calculate its area in square
-  meters.
+  使用GL draw工具，来绘制一个多边形区域。使用turf.js可以计算区域面积，面积单位为平方米。
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/mapbox-gl-draw/
