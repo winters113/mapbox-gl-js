aea0b1d32b111bdcfc6a962b2fc87f93285cc7a8
Fan <42430149+winters113@users.noreply.github.com>
2018-08-23 14:01:47 +0800

    Translation Done 100%
    line 2，3 translated

--- docs/pages/example/navigation.js
+++ docs/pages/example/navigation.js
@@ -1,6 +1,6 @@
 /*---
-title: Display map navigation controls
-description: Zoom and rotation controls to make map navigation more obvious
+title: 显示地图导航的控制选项
+description: 为使导航结果展示更清晰，可以在地图导航中使用地图放大缩小和旋转控制操作。
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/navigation/
