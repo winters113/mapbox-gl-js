42b61e9dd37a2b886cae91e3cd48ec35d3df583d
Fan <42430149+winters113@users.noreply.github.com>
2018-08-23 16:15:06 +0800

    Translation done 100%

--- docs/pages/example/popup-on-hover.js
+++ docs/pages/example/popup-on-hover.js
@@ -1,6 +1,6 @@
 /*---
-title: Display a popup on hover
-description: 'When a user hovers over a [`symbol`](/mapbox-gl-js/style-spec#layers-symbol), show a [`Popup`](/mapbox-gl-js/api/#popup) containing more information.'
+title: 在鼠标悬停在地图要素符号上时，显示弹出窗口
+description: '当用户将鼠标悬停在地图要素符号[`symbol`](/mapbox-gl-js/style-spec#layers-symbol)上时，会显示出可以展示包含更多信息的弹出窗口[`Popup`](/mapbox-gl-js/api/#popup)'
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/popup-on-hover/
