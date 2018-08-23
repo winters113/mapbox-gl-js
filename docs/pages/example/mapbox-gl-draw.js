174731826fb004451c2448c3695c33938872cfab
Fan <42430149+winters113@users.noreply.github.com>
2018-08-23 16:20:46 +0800

    Translated done 100%

--- docs/pages/example/mapbox-gl-draw.js
+++ docs/pages/example/mapbox-gl-draw.js
@@ -1,7 +1,7 @@
 /*---
 title: 绘制多边形区域 
 description: >-
- 使用 [mapbox-gl-draw](https://github.com/mapbox/mapbox-gl-draw) 绘制一个多边形。可以使用turf.js 来计算面积，面积单位为平方米。
+ 使用 [mapbox-gl-draw](https://github.com/mapbox/mapbox-gl-draw) 绘制一个多边形。可以使用[Turf.js](http://turfjs.org/)来计算多边形的面积，面积单位为平方米。
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/mapbox-gl-draw/
