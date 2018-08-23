47a3215a57b912b5cbd4e4f21dfb9d571cc7098f
Fan <42430149+winters113@users.noreply.github.com>
2018-08-23 16:19:14 +0800

    translated done 100%

--- docs/pages/example/popup-on-click.js
+++ docs/pages/example/popup-on-click.js
@@ -1,6 +1,6 @@
 /*---
 title: 鼠标单击图符时以显示弹出窗口
-description: '当用户鼠标单击地图要素符号时，显示包含更多信息的弹出窗口.'
+description: '当用户鼠标单击地图要素符号时，显示包含更多信息的弹出窗口[`Popup`](/mapbox-gl-js/api/#popup).'
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/popup-on-click/
